e4c694e55ec7ab6f21f28ce6588a770f5ebfd806
Tim Simpson <timsimpson4@gmail.com>
2017-07-22 11:15:14 -0500

    Initial commit of ModDown

---
+++ include/moddown.hpp
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------
+// moddown
+// --------------------------------------------------------------------
+//      ModDown is a tool that takes a directory representing a module
+//      of C++ code with MarkDown comments and translates it to a
+//      single MarkDown file with inline C++ code.
+//
+//      This allows you to write header files which read like
+//      documentation but remain seperated, while offering a combined
+//      view that can be rendered as HTML or otherwise styled.
+// --------------------------------------------------------------------
+namespace moddown {
+
+
+}
